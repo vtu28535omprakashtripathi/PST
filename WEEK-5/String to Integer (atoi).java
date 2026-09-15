8fbedcb7f959f021116d674e37cd4008509dfae3
vtu28535omprakashtripathi <vtu28535@veltech.edu.in>
2026-09-15 09:35:02 +0530

    Create String to Integer (atoi).java

---
+++ WEEK-5/String to Integer (atoi).java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public int myAtoi(String s) {
+
+        int i = 0;
+        int n = s.length();
+
+        // 1. Remove leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        int sign = 1;
+
+        if (i < n && s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (i < n && s.charAt(i) == '+') {
+            i++;
+        }
+
+        // 3. Convert digits
+        int result = 0;
+
+        while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+
+            int digit = s.charAt(i) - '0';
+
+            // 4. Check integer overflow
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+
+                if (sign == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+
+            result = result * 10 + digit;
+
+            i++;
+        }
+
+        return result * sign;
+    }
+}
